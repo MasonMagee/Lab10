2dca798afe2ebe1f7acbba8ad2ee431b17d1880e
MasonMagee <mwmagee42@students.tntech.edu>
2017-03-30 10:19:49 -0500

    Add files via upload
    Second Draft

--- Hybrid.h
+++ Hybrid.h
@@ -10,32 +10,32 @@ class Hybrid
 {
 
    private:
-      QueueLinked<T>* q;
-      SortedListDoublyLinked<T>* sldl;
+      QueueLinked<T>* queueLink;
+      SortedListDoublyLinked<T>* sortList;
 
    public:
       Hybrid(int (*comp_items) (T* item_1, T* item_2), int (*comp_keys) (String* key, T* item));
       ~Hybrid();
 
       bool isEmpty();
-      void enqueue(T* item);
+      void enqueue(T* item);					//add and remove items in FIFO order
       T* dequeue();
-      ListDoublyLinkedIterator<T>* iterator();
+      ListDoublyLinkedIterator<T>* iterator();	//iterate over the items in sorted order
 
 };
 
 template < class T >
 Hybrid<T>::Hybrid(int (*comp_items) (T* item_1, T* item_2), int (*comp_keys) (String* key, T* item))
 {
-   q = new QueueLinked<T>();
-   sldl = new SortedListDoublyLinked<T>(comp_items, comp_keys);
+   queueLink = new QueueLinked<T>();
+   sortList = new SortedListDoublyLinked<T>(comp_items, comp_keys);
 }
 
 template < class T >
 Hybrid<T>::~Hybrid()
 {
-   delete q;
-   delete sldl;
+   delete queueLink;
+   delete sortList;
 }
 
 //DO THIS
@@ -43,7 +43,36 @@ Hybrid<T>::~Hybrid()
 //as outlined in the Lab 10 description
 //simply comment the first implementation out when working on the second implementation
 //use the getKey method to dequeue/remove
+bool Hybrid<T>::isEmpty()
+{
+	//returns "TRUE" if the QueueLinked $ SortedListDoublyLinked has no data
+	if (!queueLink-> isEmpty()||sortList->sze == 0)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+	
+}
 
+void Hybrid<T>::enqueue(T* item)
+{
+	//Our QueueLinked||		[BACK]-Items-[FRONT]
+	//Adds a T* item to our QueueLinked & SortedListDoublyLinked, in FIFO order. So front of our stack is adding an item. 
+	//When enqueue is called on the Hybrid ADT, the item is enqueued on the queue, an O(1) operation. The item must also be added to the sorted list, an O(n) operation,
+}
+
+T* Hybrid<T>::dequeue()
+{
+	//Our QueueLinked||		[BACK]-Items-[FRONT]
+	//Takes the item in the back of our QueueLinked & SortedListDoublyLinked, and returns it. 
+}
+ListDoublyLinkedIterator* Hybrid<T>::iterator()
+{
+	//iterates through SortedListDoublyLinked
+}
 
 
 
