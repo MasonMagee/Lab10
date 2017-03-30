994ece770e72819ed3128755649625d32af93b53
MasonMagee <mwmagee42@students.tntech.edu>
2017-03-30 10:38:54 -0500

    Third

--- Hybrid.h
+++ Hybrid.h
@@ -4,6 +4,8 @@
 #include "QueueLinked.h"
 using CSC2110::QueueLinked;
 #include "SortedListDoublyLinked.h"
+#include <iostream>
+using namespace std; 
 
 template < class T >
 class Hybrid
@@ -46,10 +48,19 @@ Hybrid<T>::~Hybrid()
 bool Hybrid<T>::isEmpty()
 {
 	//returns "TRUE" if the QueueLinked $ SortedListDoublyLinked has no data
-	if (!queueLink-> isEmpty()||sortList->sze == 0)
+	
+	//This returns false if the two Links are not empty
+	if (!queueLink-> isEmpty() && sortList->sze != 0)
 	{
 		return false;
 	}
+	//This will return false and display to the users that both links are not the same size, to an error must have happened in the enque or deque process
+	else if (queueLink->sze() != sortList->sze())
+	{
+		cout<< "ERROR!!! QueueLinked and SortedListDoublyLinked are not equal in size!"
+		return false; 
+	}
+	//Else both links are empty and the Links are empty
 	else
 	{
 		return true;
@@ -62,12 +73,21 @@ void Hybrid<T>::enqueue(T* item)
 	//Our QueueLinked||		[BACK]-Items-[FRONT]
 	//Adds a T* item to our QueueLinked & SortedListDoublyLinked, in FIFO order. So front of our stack is adding an item. 
 	//When enqueue is called on the Hybrid ADT, the item is enqueued on the queue, an O(1) operation. The item must also be added to the sorted list, an O(n) operation,
+	queueLink->enqueue (item);
+	sortList ->add(item); 
 }
 
 T* Hybrid<T>::dequeue()
 {
 	//Our QueueLinked||		[BACK]-Items-[FRONT]
 	//Takes the item in the back of our QueueLinked & SortedListDoublyLinked, and returns it. 
+	if (queueLink->isEmpty())
+		return NULL;
+	
+	
+	T* item;
+	item = queueLink-> dequeue();
+	sortList-> remove (item ->getKey());
 }
 ListDoublyLinkedIterator* Hybrid<T>::iterator()
 {
